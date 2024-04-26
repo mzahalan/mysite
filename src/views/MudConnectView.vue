0667f150d61188f1c8d02f06e027ba69817496d2
Mike Zahalan <MikeZahalan@GMail.COM>
2024-04-26 00:50:57 -0700

    Added a Scrollable Buffer to the commandline

--- src/views/MudConnectView.vue
+++ src/views/MudConnectView.vue
@@ -5,11 +5,17 @@
       <v-btn v-if="sock" @click="handleClose">Disconnect!</v-btn>
       <span v-if="spinner">Connecting...</span>
     </div>
-    <div id="mudbox" class="overflow-auto flex-grow-1">
+    <div id="mudbox" class="overflow-auto flex-grow-1 pl-5">
       <div class="content ma-0 pa-0" v-for="msg in messages" :key="msg" v-html="msg"></div>
     </div>
     <div class="shrink">
-      <v-text-field v-model="message" @keyup.enter="sendMessage" class="flex-1-1-100">
+      <v-text-field 
+          ref="commandLine"
+          v-model="message" 
+          @keyup.enter="sendMessage" 
+          @keyup.arrow-up="handleScroll(-1)"
+          @keyup.arrow-down="handleScroll(1)"
+          class="flex-1-1-100">
         <template v-slot:append-inner>
           <button @click="sendMessage"><v-icon>mdi-send</v-icon></button>
         </template>
@@ -22,8 +28,10 @@
 import { nextTick, ref, watch } from 'vue'
 const spinner = ref(false)
 const sock = ref(null)
+const commandLine = ref(null)
 const messages = ref([])
 const message = ref('')
+
 
 const handleClick = () => {
   spinner.value = true
@@ -50,11 +58,40 @@ watch(
   { deep: true }
 )
 
+const history = []
+const MAX_HISTORY = 50
+let historyPointer = -1
+
+const handleScroll = (inc) => {
+  if(history.length > 0) {
+    historyPointer = (historyPointer + inc) % history.length
+    
+    if(historyPointer < 0) {
+      historyPointer = history.length -1
+    }
+
+    message.value = history[historyPointer]
+    nextTick(() => {
+      commandLine.value.select()
+    })
+  }
+}
+
+const addHistory = (msg) => {
+  if(msg.trim() != '') {
+    history.push(msg)
+    if(history.length > MAX_HISTORY) {
+      history.shift()
+    }
+    historyPointer = history.length - 1
+  }
+}
 const sendMessage = () => {
   let msg = message.value
+  addHistory(msg)
   if (sock.value) {
     sock.value.send(msg)
-    message.value = ''
+    commandLine.value.select()
   }
 }
 </script>
